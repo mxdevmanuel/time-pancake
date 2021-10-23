350cc3ef8dfbe3e3c8079c8300a5121c83f29ac8
Manuel Morales <p513manu@gmail.com>
2021-10-23 16:55:37 -0500

    feat: careless whisper

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,63 @@
-import Head from 'next/head'
-
+import Image from "next/image";
+import { HeroLocation } from "@components/icons";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="flex flex-col lg:flex-row min-h-screen">
+      <div className="w-full lg:w-1/3 bg-navy flex flex-col items-center">
+        <div className="w-full flex flex-row justify-between px-8 mt-8 justify-self-start">
+          <button className="bg-gray-500 shadow-lg text-white px-4 py-2">
+            Search for places
+          </button>
+          <button className="rounded-full shadow-lg bg-gray-500 p-2 text-white">
+            <HeroLocation />
+          </button>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className="container h-full flex "
+          style={{
+            backgroundImage: "url(/cloud_pattern.svg)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 120%",
+          }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          <img src="/Shower.png" className="m-auto" />
+        </div>
+        <div className="flex flex-col justify-between p-5 h-full">
+          <h2 className="text-9xl text-gray-300 text-center">
+            15<span className="text-7xl text-gray-400">˚c</span>
+          </h2>
+          <span className="text-4xl text-gray-400 text-center my-auto">
+            Shower
+          </span>
+          <span className="text-lg text-gray-400 text-center my-3">
+            Today · Fri, 5 Jun
+          </span>
+          <span className="text-lg text-gray-400 text-center flex place-content-center">
+            <HeroLocation className="h-5 w-5 mt-1" />
+            Helsinki
+          </span>
+        </div>
+      </div>
+      <div className="w-full lg:w-2/3 bg-navy-dark flex flex-col justify-start items-center p-10">
+        <div className="flex flex-row self-end text-2xl">
+          <span className="m-3 px-3 py-2 rounded-full bg-gray-300 font-bold">
+            ˚C
+          </span>
+          <span className="m-3 px-3 py-2 rounded-full bg-navy text-white font-bold">
+            ˚F
+          </span>
+        </div>
+        <div className="flex flex-row justify-evenly">
+          <div className="w-full mx-2 bg-navy text-white flex flex-col">
+            <span>Tomorrow</span>
+            <img src="/Shower.png" className="m-auto" />
+            <div className="grid grid-cols-2 gap-3">
+              <span>16˚c</span>
+              <span>11˚c</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          light: "#737bb7",
+          DEFAULT: "#1E213A",
+          dark: "#100E1D",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
